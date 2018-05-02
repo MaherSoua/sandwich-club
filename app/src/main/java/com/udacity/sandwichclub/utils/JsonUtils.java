14b4dad5288b71879253a90451f5f36920873118
MaherSoua <mahersoua@gmail.com>
2018-05-03 01:37:47 +0200

    Improve error exception, formatting code

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -5,17 +5,19 @@ import android.util.Log;
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
+    private final static String TAG = "JsonUtils";
 
     public static Sandwich parseSandwichJson(String json) {
-        try{
+        try {
             JSONObject jsonObject = new JSONObject(json);
-            JSONObject nameObject =  jsonObject.getJSONObject("name");
+            JSONObject nameObject = jsonObject.getJSONObject("name");
             String name = nameObject.getString("mainName");
             List<String> knownAs = setArray(nameObject.getJSONArray("alsoKnownAs"));
 
@@ -24,21 +26,22 @@ public class JsonUtils {
             String image = jsonObject.getString("image");
             List<String> ingredients = setArray(jsonObject.getJSONArray("ingredients"));
             return new Sandwich(name, knownAs, placeOfOrigin, description, image, ingredients);
-        } catch (Exception error){
-            Log.d("JSONuTILS" , error.getMessage());
+        } catch (JSONException error) {
+            Log.e(TAG, error.getMessage());
             return null;
         }
 
     }
 
-    private static List<String> setArray(JSONArray jsonArray){
+    private static List<String> setArray(JSONArray jsonArray) {
         List<String> list = new ArrayList<>();
-        if(jsonArray != null){
+        if (jsonArray != null) {
             try {
-                for( int i = 0; i < jsonArray.length(); i++){
+                for (int i = 0; i < jsonArray.length(); i++) {
                     list.add(jsonArray.getString(i));
                 }
-            } catch (Exception error){
+            } catch (JSONException error) {
+                Log.e(TAG, error.getMessage());
 
             }
         }
